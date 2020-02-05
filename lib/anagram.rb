c445f40aa8578334cf25b23512daa2fe506b9cb1
Roberto Livi <robertomlivi@outlook.com>
2020-02-05 14:57:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  new_array = []
+  array.collect do |i|
+  if i.chars.sort == word.chars.sort
+      new_array << i
+end
+ end
+ new_array
+end
+end
